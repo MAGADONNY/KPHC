547437452f65e49b05e4fd470daf626746d55a81
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-06-18 13:56:43 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -186,3 +186,10 @@ if df is not None:
         st.button(t_dugme_obrisi, on_click=isprazni_dnevnik_callback)
 else:
     st.error("Baza podataka 'KPH-AI.xlsx' nije pronađena ili je oštećena.")
+
+# --- FUTER SA INFORMACIJAMA I BROJAČEM POSETA ---
+st.write("---")
+st.markdown("<p style='text-align: center; color: #888888; font-size: 14px;'>Ukupno poseta aplikaciji: 3184</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-weight: bold; color: #ffffff;'>♣️♦️♥️♠️ MAGICOMP & AI Gemini</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #279FF5;'>magy@usa.com &nbsp;&nbsp;|&nbsp;&nbsp; Tel.+38163310850</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-style: italic; color: #888888; font-size: 12px;'>Powered by PYTHON</p>", unsafe_allow_html=True)
