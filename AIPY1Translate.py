bf10b8cf370052773c77294c7e406b07ff231b4e
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-06-19 23:43:20 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -138,7 +138,7 @@ def generisi_pdf_file(ime_pacijenta, godina_rodjenja, df_podaci, uk_k, uk_f, uk_
     pdf.set_font("Helvetica", "B", 10)
     pdf.cell(120, 6, text=f" IME I PREZIME / GODINA RODJENJA: {ime_pacijenta.upper()} ({godina_rodjenja})", border=0, ln=0)
     pdf.set_font("Helvetica", size=10)
-    pdf.cell(60, 6, text=f"DATUM: {datetime.now().strftime('%d.%m.%Y.')} ", border=0, ln=1, align="R")
+    pdf.cell(40, 6, text=f"DATUM: {datetime.now().strftime('%d.%m.%Y.')} ", border=0, ln=1, align="R")
     pdf.ln(6)
     
     # 3. Tabela obroka (Zeleno-sivo zaglavlje)
