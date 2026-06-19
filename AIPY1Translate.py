3a94c4fbcdc65ea6c306cf4ad73a3168cb4ff911
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-06-19 23:41:41 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -125,7 +125,7 @@ def generisi_pdf_file(ime_pacijenta, godina_rodjenja, df_podaci, uk_k, uk_f, uk_
     # Naslov unutar zelene trake
     pdf.set_text_color(100, 100, 100)
     pdf.set_font("Helvetica", "B", 14)
-    pdf.cell(180, 11, text="DNEVNIK ISHRANE & UNOSA MINERALA", border=0, ln=1, align="C")
+    pdf.cell(180, 15, text="DNEVNIK ISHRANE & UNOSA MINERALA", border=0, ln=1, align="C")
     
     # Vraćamo tekst na tamno sivu
     pdf.set_text_color(44, 62, 80)
