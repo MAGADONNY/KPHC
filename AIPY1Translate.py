b0de3de2b7fa9758e9dff913e8cedab9ea52a1d3
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-06-20 23:32:58 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -217,7 +217,7 @@ def generisi_pdf_file(ime_pacijenta, godina_rodjenja, df_podaci, uk_k, uk_f, uk_
     # Smanjena visina zaglavlja sa 8 na 5.5
     pdf.cell(40, 5.5, " Mineral", 1, 0, "C", True)
     pdf.cell(45, 5.5, "Ukupno uneto", 1, 0, "C", True)
-    pdf.cell(45, 5.5, "Dozvoljeni limit", 1, 0, "C", True)
+    pdf.cell(45, 5.5, "Preporuceni limit", 1, 0, "C", True)
     pdf.cell(50, 5.5, "Status", 1, 1, "C", True)
     
     pdf.set_font("Courier", "", 10)
