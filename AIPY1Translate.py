6ddb5c38171f6244e685e34f581b1ec7a950a600
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-06-20 20:56:54 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -162,20 +162,22 @@ def generisi_pdf_file(ime_pacijenta, godina_rodjenja, df_podaci, uk_k, uk_f, uk_
 
 
 
-        # 3. Tabela obroka (Zeleno-sivo zaglavlje)
+    # 3. Tabela obroka (Zeleno-sivo zaglavlje)
     pdf.set_font("Courier", "", 10)
     pdf.set_fill_color(52, 73, 94) 
     pdf.set_text_color(255, 255, 255) 
     
-    pdf.cell(65, 9, text=" Namirnica", border=0, fill=True)
-    pdf.cell(25, 9, text="Kolicina (g)", border=0, fill=True, align="C")
-    pdf.cell(30, 9, text="Kalijum (mg)", border=0, fill=True, align="C")
-    pdf.cell(30, 9, text="Fosfor (mg)", border=0, fill=True, align="C")
-    pdf.cell(30, 9, text="Natrijum (mg)", border=0, fill=True, align="C")
+    # Širine su vraćene na staro, a visina je smanjena sa 9 na 6.3
+    pdf.cell(65, 6.3, text=" Namirnica", border=0, fill=True)
+    pdf.cell(25, 6.3, text="Kolicina (g)", border=0, fill=True, align="C")
+    pdf.cell(30, 6.3, text="Kalijum (mg)", border=0, fill=True, align="C")
+    pdf.cell(30, 6.3, text="Fosfor (mg)", border=0, fill=True, align="C")
+    pdf.cell(30, 6.3, text="Natrijum (mg)", border=0, fill=True, align="C")
     pdf.ln()
     
     pdf.set_text_color(44, 62, 80)
     pdf.set_font("Helvetica", size=10)
+
 
     
     
