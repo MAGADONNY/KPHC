98ad64d0f592134bb6882dbb4fc43ec82f75b463
MAGADONNY <34655300+MAGADONNY@users.noreply.github.com>
2026-08-07 22:43:59 +0200

    Update AIPY1Translate.py

--- AIPY1Translate.py
+++ AIPY1Translate.py
@@ -608,11 +608,12 @@ import os
 import streamlit as st
 
 # --- NOVI GITHUB BROJAČ POSETA ---
-    from github import Github, Auth
-    auth = Auth.Token(st.secrets["GITHUB_TOKEN"])
-    g = Github(auth=auth)
-    repo = g.get_repo("MAGADONNY/KPHC")
-    file_content = repo.get_contents("brojac.txt")
+from github import Github, Auth
+auth = Auth.Token(st.secrets["GITHUB_TOKEN"])
+g = Github(auth=auth)
+repo = g.get_repo("MAGADONNY/KPHC")
+file_content = repo.get_contents("brojac.txt")
+
 #----------Kraj Tokena-----------
     
     trenutni_broj = int(file_content.decoded_content.decode("utf-8"))
